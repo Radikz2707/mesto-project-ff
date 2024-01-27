d85fca059b796b387b8b110d6ea9073701ac0e41
Radik Zalalov <radikz1977@yandex.ru>
2024-01-27 10:03:35 +0300

    Небольшие исправления

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,33 @@
-'use strict'
-// @todo: Темплейт карточки
+'use strict';
 
-// @todo: DOM узлы
+const cardList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
-
-const cardList = document.querySelector('.places__list')
 function createCard(card, removeCard) {
-	const cardTemplate = document.querySelector('#card-template').content
-	const cardElement = cardTemplate.querySelector('.card').cloneNode(true)
-	const cardImage = cardElement.querySelector('.card__image')
-	cardImage.src = card.link
-	cardImage.alt = card.name
-	const cardTitle = cardElement.querySelector('.card__title')
-	cardTitle.textContent = card.name
-	const removeButton = cardElement.querySelector('.card__delete-button')
-	const removeButtonClick = evt => {
-		const cardItem = evt.target.closest('.card')
-		removeCard(cardItem)
-	}
-	removeButton.addEventListener('click', removeButtonClick)
-	return cardElement
+	const cardTemplate = document.querySelector('#card-template').content;
+	const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+	const removeButton = cardElement.querySelector('.card__delete-button');
+	const cardTitle = cardElement.querySelector('.card__title');
+	const cardImage = cardElement.querySelector('.card__image');
+	cardImage.src = card.link;
+	cardImage.alt = card.name;
+	cardTitle.textContent = card.name;
+
+	// Функция слушателя реализация 1ый способ
+	// const removeButtonClick = evt => {
+	// 	const cardItem = evt.target.closest('.card')
+	// 	removeCard(cardItem)
+	// }
+
+	// Функция слушателя реализация 2ой способ
+	const removeButtonClick = () => removeCard(cardElement);
+
+	removeButton.addEventListener('click', removeButtonClick);
+	return cardElement;
 }
-function removeCard(cardNode) {
-	cardNode.remove()
+function removeCard(cardNode)	{
+	cardNode.remove();
 }
-initialCards.forEach(function (card) {
-	const newCard = createCard(card, removeCard)
-	cardList.append(newCard)
+initialCards.forEach((card) => {
+	const newCard = createCard(card, removeCard);
+	cardList.append(newCard);
 })
