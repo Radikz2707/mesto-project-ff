ac800950e1833f348944ac3cbdac0cd8d5868749
Radik Zalalov <radikz1977@yandex.ru>
2024-02-07 14:02:22 +0300

    Продолжение работы

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -38,27 +38,43 @@ initialCards.forEach(card => {
 
 const pageContent = document.querySelector('.page__content');
 const popupEdit = pageContent.querySelector('.popup_type_edit');
+const popupEditForm = popupEdit.querySelector('.popup__form');
+let nameInput = popupEditForm.querySelector('.popup__input_type_name');
+let jobInput = popupEditForm.querySelector('.popup__input_type_description');
 const popupNew = pageContent.querySelector('.popup_type_new-card');
 const profileInfo = pageContent.querySelector('.profile');
 const profileEditButton = profileInfo.querySelector('.profile__edit-button');
 const profileAddButton = profileInfo.querySelector('.profile__add-button');
+const profileTitle = profileInfo.querySelector('.profile__title');
+const profileJob = profileInfo.querySelector('.profile__description');
 const popups = Array.from(document.querySelectorAll('.popup'));
-let target; // в него будет падать открытый popup
+const cardsSection = document.querySelector('.places__list');
+const cards = Array.from(cardsSection.querySelectorAll('.card'));
+let target = ''; // в него будет падать открытый popup
+nameInput.value = profileTitle.textContent;
+jobInput.value = profileJob.textContent;
 
 function openPopup(popup) {
+	// функция для открытия попапа
 	popup.classList.add('popup_is-opened');
+	document.addEventListener('keydown', keyChecker);
 	return popup;
 }
 function closePopup(popup) {
+	// функция для закрытия попапа
 	popup.classList.remove('popup_is-opened');
+	target = '';
+	document.removeEventListener('keydown', keyChecker);
 }
 function keyChecker(evt) {
+	// функция для закрытия попапа по кнопке ESC
 	if (evt.keyCode === 27) {
 		target = popupChecker();
 		closePopup(target);
 	}
 }
 function popupChecker() {
+	// функция для определения попапа, который открыт в данный момент
 	popups.forEach(function (popup) {
 		if (popup.classList.contains('popup_is-opened')) {
 			target = popup;
@@ -66,14 +82,22 @@ function popupChecker() {
 	});
 	return target;
 }
-function clickChecker(evt) {
-	target = popupChecker();
-	const closeButton = target.querySelector('.popup__close');
-	const popupContent = target.querySelector('.popup__content');
-	if ((evt.target === closeButton || evt.target === target) && evt.target !== popupContent) {
+function clickChecker() {
+	target = popupChecker(); // открытый попап, если он открыт, то он будет в target
+	if (target === '') {
+		// если ни один попап не открыт...
+		return;
+	} else {
+		const closeButton = target.querySelector('.popup__close'); // кнопка в открытом попапе
+		popupListener(closeButton, target);
 		closePopup(target);
 	}
 }
+function popupListener(closeButton, target)	{
+	closeButton.addEventListener('click', clickChecker)
+	target.addEventListener('click', clickChecker);
+}
+
 profileEditButton.addEventListener('click', function (evt) {
 	evt.stopPropagation();
 	target = openPopup(popupEdit);
@@ -83,5 +107,15 @@ profileAddButton.addEventListener('click', function (evt) {
 	target = openPopup(popupNew);
 });
 
-document.addEventListener('keydown', keyChecker);
 pageContent.addEventListener('click', clickChecker);
+
+function handleEditFormSubmit(evt) {
+	evt.preventDefault();
+	let name = nameInput.value;
+	let job = jobInput.value;
+	profileTitle.textContent = name;
+	profileJob.textContent = job;
+	target = popupChecker();
+	closePopup(target);
+}
+popupEditForm.addEventListener('submit', handleEditFormSubmit);
